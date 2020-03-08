9f0659d44698759c3ed9bf4c612f78a1fd4d6b01
harrycampaz <harrycampaz@gmail.com>
2020-03-08 18:04:58 -0500

    done commit

--- app/src/main/java/com/dezzapps/fluitapplication/data/FruitDataSet.kt
+++ app/src/main/java/com/dezzapps/fluitapplication/data/FruitDataSet.kt
@@ -9,7 +9,10 @@ class FruitDataSet {
         return listOf(
             Fruit("manzana", "Rojo", 1000.0f, 200.0f),
             Fruit("Pera", "Verde", 1200.0f, 300.0f),
-            Fruit("Banana", "Amarillo", 100.0f, 100.0f))
+            Fruit("Banana", "Amarillo", 100.0f, 100.0f),
+        Fruit("Uvas", "Morado", 500.0f, 100.0f),
+        Fruit("Naranja", "Amarillo", 700.0f, 100.0f),
+        Fruit("melocoton", "amarillo", 200.0f, 100.0f))
 
     }
 }
